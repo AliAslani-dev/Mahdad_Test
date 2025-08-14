58501e213fb0ce4328ef91a0e59f317b0463fa36
AliAslani1234 <ali_aslaniii10@yahoo.com>
2025-08-14 16:33:05 -0700

    first commit

---
+++ src/layouts/DefaoultLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+      </nav>
+    </header>
+    <main>
+      <div><router-view /></div>
+    </main>
+    <footer></footer>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
